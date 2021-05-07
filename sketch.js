5b72e66c620b6e68a7f12ac5b607d33d530823d4
dakshesh9870 <77739671+dakshesh9870@users.noreply.github.com>
2021-05-07 19:55:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+
+
+
+function preload(){
+ //load your images here 
+ 
+  
+}
+
+function setup() {
+  createCanvas(600, 600);
+  
+  //add code here
+  
+}
+
+function draw() {
+
+  //add code here
+  
+}
+
